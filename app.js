b31b96689dc478524ea3b0e313195479d10c6f12
aksh <sisodiyaaksh@gmail.com>
2022-09-14 01:15:11 +0530

    db added

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
+const apiRoutes = require('./routes/api')
 const app = express()
+
+app.use('/api', apiRoutes)
+
+// connect to db
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {useNewUrlParser:true}, 
+    ()=>console.log("DB!!!"))
+
+
+// run server
+const port = process.env.PORT
+app.listen(port, ()=>console.log(`listening on port ${port}`))
